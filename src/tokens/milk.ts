20e50e68d6febd0d4e75a3ae280b855e94e2a090
MuesliSwapTeam <muesliswap@gmail.com>
2022-11-23 19:34:15 +0100

    Update milk treasury

--- src/tokens/milk.ts
+++ src/tokens/milk.ts
@@ -7,7 +7,8 @@ const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 10_000_000n;
   const treasury = await getAmountInAddresses(blockFrost, MILK, [
-    "addr1v8c3mztrzpjqxzrcl8rvxln8xyvanz6pufuaju7rwkglnychv3cg3",
+    "addr1v8c3mztrzpjqxzrcl8rvxln8xyvanz6pufuaju7rwkglnychv3cg3", // treasury
+    "addr1v8h4fm4ejd9w8wr8lkkeu0pe4m00ycl2vysd3jvs9mgw7ps8sm9rt", // yield rewards
   ]);
   return {
     circulating: (total - treasury).toString(),
